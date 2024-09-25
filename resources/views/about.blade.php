d0aa529287a2ef8a2b5108ee0313036bee7598a7
LiteralHat <literalhatreloaded@gmail.com>
2024-09-25 22:22:31 +1000

    passive aggressive homepage and layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>About Page</h1>
-</body>
-</html>
+<x-navbar>
+<h2>About.</h2>
+
+<p>I make things.</p>
+
+<h3>What happened to the old site?</h3>
+
+<p>It was too complex and painful to maintain. For now I am focusing on functionality and back-end. If you need pretty graphics to catch your attention at all times, this website may be too avant-garde for your taste and I would recommend TickTocks as an alternative.</p>
+
+<h3>Can I use your art in _______?</h3>
+
+<p>As long as your use isnt monetary, you're free to use any of my work in profile pictures, videos, remixes, etc. If unsure, then probably best not to.</p>
+
+
+</x-navbar>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>LiteralHat.Com</h1>
+    <p>Yes, this is my new website. Are you mad? I don't care.</p>
+    <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/gallery">Gallery</a></li>
+    </ul>
+
+{{ $slot }}
+
+</body>
+</html>
